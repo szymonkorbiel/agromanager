5efdc4d871ad765e4726c05a620ca013503c5fdb
szymonkorbiel <szymon.korbiel@gmail.com>
2023-06-26 23:46:44 +0200

    Add new files

--- src/App.js
+++ src/App.js
@@ -19,9 +19,31 @@ function App() {
 
       <div className="container">
         {/* Nawigacja */}
-        <nav>
-          {/* Umieść swoje elementy nawigacyjne tutaj */}
-        </nav>
+        <div class="items">
+          <div class="items-head">
+            <p>UX DESIGN</p>
+            
+          </div>
+          
+          <div class="items-body">
+            <div class="items-body-content">
+              <span>Web Usabilty Testing</span>
+              <i class="fa fa-angle-right"></i>
+            </div>
+            <div class="items-body-content">
+              <span>Design of Everyday Things</span>
+              <i class="fa fa-angle-right"></i>
+            </div>
+            <div class="items-body-content">
+              <span>Practical Empathy: For Collaboration & Creativity in Your Work</span>
+              <i class="fa fa-angle-right"></i>
+            </div>
+            <div class="items-body-content">
+              <span>About Face: The Essentials of Interaction Design</span>
+              <i class="fa fa-angle-right"></i>
+            </div>
+          </div>
+        </div>
 
         {/* Główna zawartość */}
         <main>
@@ -30,8 +52,13 @@ function App() {
       </div>
 
       <footer className="App-footer">
-        {/* Umieść swoje elementy stopki tutaj */}
+          <p>Copyright</p>
+        <div>
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+        </div>
       </footer>
+
     </div>
   );
 }
