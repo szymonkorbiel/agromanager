aba10f352adc01fa05f617024e4e74912435c43d
szymonkorbiel <szymon.korbiel@gmail.com>
2023-06-26 23:12:48 +0200

    Add new files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* Logo i nazwa */}
+        <div className="header-left">
+          <img src="logo.png" alt="Logo" />
+          <h1>AgroManager</h1>
+        </div>
+
+        {/* Nazwa użytkownika */}
+        <div className="header-right">
+          <p>Username</p>
+        </div>
       </header>
+
+      <div className="container">
+        {/* Nawigacja */}
+        <nav>
+          {/* Umieść swoje elementy nawigacyjne tutaj */}
+        </nav>
+
+        {/* Główna zawartość */}
+        <main>
+          {/* Umieść swoje elementy głównej zawartości tutaj */}
+        </main>
+      </div>
+
+      <footer className="App-footer">
+        {/* Umieść swoje elementy stopki tutaj */}
+      </footer>
     </div>
   );
 }
